cbac21c30dfb2f4fa2700f26e6f2ea48db727b98
thiennguyen1473 <thiennguyen1473@gmail.com>
2026-09-16 15:42:28 +0700

    Update SQL queries

--- SQL/create_table.sql
+++ SQL/create_table.sql
@@ -62,6 +62,14 @@ create table Product(
 foreign key (`sub-Cate id`) references SubCate(`sub-Cate id`)
 );
 
+create table Product_test(
+`product id` varchar(255) primary key not null,
+`product Name` varchar(255),
+`sub-Cate id` int,
+`Unit Cp` float,
+`Unit Sp` float
+);
+
 
 create table RetaildSalesPeople(
 `retail Sales People id` int primary key not null,
@@ -85,7 +93,6 @@ create table FactRetaild(
 `product id` varchar(255),
 
 foreign key(`order Date`) references Calendar(`date`),
-foreign key(`ship Date`) references Calendar(`date`),
 foreign key(`ship Mode id`) references ShipMode(`ship Mode id`),
 foreign key(`customer id`) references Customer(`customer id`),
 foreign key(`postal Code`) references Location(`postal Code`),
@@ -93,6 +100,8 @@ foreign key(`product id`) references Product(`product id`),
 foreign key(`retail Sales People id`) references RetaildSalesPeople(`retail Sales People id`)
 
 );
+
+
 
 alter table factRetaild
 add returned varchar(255),
@@ -117,12 +126,12 @@ select count(distinct `order id`) from factretaild;
 select sum(sales) - sum(cost) as loinhuan from factretaild;
 
 -- doanh thu theo năm
-select c.year, sum(f.sales) from factretaild f join calendar c
+select c.year, sum(f.sales) as tong from factretaild f join calendar c
 on f.`order date` = c.`date`
 group by c.year;
 
 -- giá trung bình của tất cả hàng
-select avg(sales) as avg_price from factretaild ;
+select sum(sales)/sum(quantity) as avg_price from factretaild ;
 
 -- doanh thu theo tháng
 select c.month,c .year, sum(f.sales) as tong_month from factretaild f join calendar c
@@ -131,11 +140,14 @@ group by c.month,c.year
 order by c.year, c.month;
 
 # doanh thu và lợi nhuận cao nhất
-select c.`category`, sum(f.sales) from factretaild f join product p
+select c.`category`, sum(f.sales) as total_sales,(sum(f.sales) - sum(f.cost)) as total_profit from factretaild f join product p
 on f.`product id` = p.`product id`
 join subcate s on p.`sub-Cate id` = s.`sub-Cate id`
 join category c on s.`category id` = c.`category id`
-group by c.`category`;
+group by c.`category`
+order by total_profit desc;
+
+
 
 #sub-cate doanh thu cao nhất 
 select s.`sub-cate id`, sum(f.sales) from factretaild f join product p on f.`product id`
@@ -160,19 +172,20 @@ group by sh.`ship mode id`,sh.`ship mode`;
 
 
 # khu vực doanh thu cao nhất
-select l.`postal code`, l.`country`, sum(f.sales) as tong from factretaild f 
+ select l.`postal code`, l.`country`, sum(f.sales) as tong from factretaild f 
 join location l on f.`postal Code` = l.`postal code` 
 group by l.`country`, l.`postal Code`
 order by tong desc
-limit 1;
+limit 10;
 
 
-# nhân viên doanh thu cao nhat
-select c.`customer id`, c.`customer name`, sum(f.sales) as tong from factretaild f 
-join customer c on f.`customer id` = c.`customer id`
-group by c.`customer id`, c.`customer name`
-order by tong desc
-limit 1;
+
+# 10 nhân viên doanh thu cao nhat
+select r.`retail sales people id`, r.`retaild sales people`, sum(f.sales) as tong from factretaild f 
+join retaildsalespeople r on f.`retail Sales People id` = r.`retail Sales People id`
+group by r.`retail sales people id`, r.`retaild Sales People`
+order by tong desc;
+
 
 # doanh thu các năm so với năm trước đó
 select c.year, sum(f.sales), sum(f.sales) - lag(sum(f.sales)) over (order by c.year) as distince from factretaild f join calendar c
@@ -181,21 +194,23 @@ group by c.year
 order by distince desc;
 
 # sản phẩm chưa từng bán
-select c.`category id`,s.`sub-cate id`,p.`product id`, p.`product name`,f.`retail Order id` from product p left join factretaild f on p.`product id` = f.`product id`
-right join subcate s on s.`sub-cate id` = p.`sub-cate id` right join category c on c.`category id` = s.`category id`
-where p.`product id` is null;
+select c.`category id`,s.`sub-cate id`,p.`product id`, p.`product name`,f.`retail Order id` from product p 
+left join factretaild f on p.`product id` = f.`product id`
+join subcate s on s.`sub-cate id` = p.`sub-cate id` 
+join category c on c.`category id` = s.`category id`
+where f.`product id` is null;
 
 #khách hàng chưa đặt đơn nào
-select c.`customer id`,f.`retail order id` from factretaild f right join customer c on f.`customer id` = c.`customer id`;
+select c.`customer id`,f.`retail order id` from factretaild f right join customer c on f.`customer id` = c.`customer id` where f.`retail Order id` is null;
 
 
 #top 3 san pham doanh thu cao nhat 
-with cte as (select p.`product id`, p.`product name`, sum(f.sales) as tong, 
+with cte as (select c.`category id`,p.`product id`, p.`product name`, sum(f.sales) as tong, 
 rank() over(partition by c.`category` order by sum(f.sales) desc ) as xep_hang
 from factretaild f join product p on f.`product id` = p.`product id`
 join subcate s on p.`sub-cate id` = s.`sub-cate id` 
 join category c on c.`category id` = s.`category id`
-group by p.`product name`, p.`product id` )
+group by c.`category id`, p.`product name`, p.`product id` )
 select * from cte where xep_hang <=3;
 
 
@@ -213,7 +228,6 @@ with cte as(select s.`sub-category`, c.`customer name`, c.`customer id`, sum(f.s
 join customer c on f.`customer id` = c.`customer id`
 join product p on p.`product id` = f.`product id` 
 join subcate s on s.`sub-cate id` = p.`sub-cate id` 
-group by s.`sub-category`,c.`customer name`, c.`customer id`) select `customer name`, `customer id`, `sub-category`,tong_dthu  from cte where tong_dthu <= 10 
-
+group by s.`sub-category`,c.`customer name`, c.`customer id`) select `customer name`, `customer id`, `sub-category`,tong_dthu  from cte where tong_dthu <= 10 ;
 
 
